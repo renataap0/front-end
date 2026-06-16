47cf80cc6e278a387d3e4ddf97b32094c9ab0a53
renataap0 <aparecidadequeirozpererrenata@gmail.com>
2026-06-16 14:11:10 -0300

    Delete back-end directory

--- back-end/src/routes/authRoutes.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import * as authController from "../controllers/authController";
-
-export const authRoutes = Router();
-
-authRoutes.post("/login", authController.login);
